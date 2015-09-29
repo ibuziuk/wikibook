d139b3e57c459b104206678e62ddf3caa3500945
Ilya Buziuk <ibuziuk@exadel.com>
2015-09-29 18:28:52 +0300

    Implementing Live Search on IMDB

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	console.log( "ready!" );
+	$('#search').keyup(function () {	 
+		var searchField = $('#search').val();
+		var url = "http://www.omdbapi.com/?s=" + searchField;
+		$.getJSON(url, function (data) {
+			var output = '<ul class="searchresult">';
+			var resultSet = data.Search;
+			if (resultSet) {
+				$.each(resultSet, function(key, val) {
+					output +='<li id="' + val.Title + '">';
+					output +='<h2>' + val.Title + '</h2>';
+					output +='<p>' + val.Year + '</p>';
+					output +='</li>';
+				});
+				$('#update').html(output);		
+				$(".searchresult li").each(function() {
+					var root = this;
+					// if there is no 'img' tag, try to load image 
+					if (!root.childNodes[2]) {
+						var title = this.id;
+						var url = "http://www.omdbapi.com/?t=" + title;
+						
+						$.getJSON(url, function (data) {
+							var poster = data.Poster;
+							if (poster && poster !== "N/A") {
+								var img = document.createElement('img');
+								img.setAttribute('src', poster);
+								root.appendChild(img);
+							}
+						});
+					}
+				});
+			}
+		});
+	});
+});
