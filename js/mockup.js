7711b928311c9fa5689e0db9db8aebc49ed2dd66
Ilya Buziuk <ibuziuk@exadel.com>
2015-10-15 16:06:41 +0300

    Adding moview via separate section

--- js/mockup.js
+++ js/mockup.js
@@ -13,20 +13,42 @@ $(document).ready(function() {
 		}
 	}
 
+	function saveMovie() {
+		var movieUrl = $("#m_url").val();
+		var year = $("#m_year").val();
+		var movieDir = $("#d_url").val();
+
+		var url_chunks = movieUrl.split("/");
+		var title = url_chunks[url_chunks.length - 1].replace("_", " ");
+
+		var dir_url_chunks = movieDir.split("/");
+		var dirTitle = dir_url_chunks[dir_url_chunks.length -1].replace("_", " ");
+
+		var movie = "<a href='" + movieUrl +"' target='_blank'>" + title + "</a>"
+		var dir = "<a href='" + movieDir +"' target='_blank'>" + dirTitle + "</a>"
+
+		$("#m_table > tbody").append("<tr><td>" + movie + "</td><td>" + year + "</td><td>" + dir + "</td></tr>");
+	}
+
 	$("#toggle_content").click(function() {
 		toggle(this, $("#content_list"));
 	});
 
 	$("#toggle_categories").click(function() {
-		toggle(this, $("#category_list"));
+		toggle(this, $("#new_moview"));
 	});
 
 	$("#toggle_movies").click(function() {
 		toggle(this, $("#movie_list"));
+
 	});
 
 	$("#toggle_books").click(function() {
 		toggle(this, $("#book_list"));
+	});
+
+	$("#add").click(function() {
+		saveMovie();
 	});
 
 });
